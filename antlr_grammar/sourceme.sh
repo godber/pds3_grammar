4e3a8f877dc72f1ec94a06f5d41b778b4bc0279e
Austin Godber <godber@uberhip.com>
2014-12-31 10:26:45 -0700

    added diagrun and changed functions to use grun variable instead of alias

--- antlr_grammar/sourceme.sh
+++ antlr_grammar/sourceme.sh
@@ -2,23 +2,34 @@ export CLASSPATH=".:/usr/local/lib/antlr-4.4-complete.jar:$CLASSPATH"
 alias antlr4='java -jar /usr/local/lib/antlr-4.4-complete.jar'
 alias grun='java org.antlr.v4.runtime.misc.TestRig'
 
+export GRUN_CMD='java org.antlr.v4.runtime.misc.TestRig'
+
+diagrun() {
+  testfile=`readlink -f $1`
+  cd build-java/ && $GRUN_CMD ODLv21 label -diagnostics < $testfile
+  cd ..
+}
 
 guirun() {
   testfile=`readlink -f $1`
-  cd build-java/ && grun ODLv21 label -gui < $testfile && cd ..
+  cd build-java/ && $GRUN_CMD ODLv21 label -gui < $testfile
+  cd ..
 }
 
 tokenrun() {
   testfile=`readlink -f $1`
-  cd build-java/ && grun ODLv21 label -tokens < $testfile && cd ..
+  cd build-java/ && $GRUN_CMD ODLv21 label -tokens < $testfile
+  cd ..
 }
 
 treerun() {
   testfile=`readlink -f $1`
-  cd build-java/ && grun ODLv21 label -tree < $testfile && cd ..
+  cd build-java/ && $GRUN_CMD ODLv21 label -tree < $testfile
+  cd ..
 }
 
 tracerun() {
   testfile=`readlink -f $1`
-  cd build-java/ && grun ODLv21 label -trace < $testfile && cd ..
+  cd build-java/ && $GRUN_CMD ODLv21 label -trace < $testfile
+  cd ..
 }
